330abf6cbaa3f9b0905854a160ad2b5d4b3839c8
DanielJyc <496830205@qq.com>
2014-07-21 10:55:29 +0800

    Add multi-Datanode

--- HDFS.py
+++ HDFS.py
@@ -1,15 +1,21 @@
+import os
+
 class Datanode(object):
 	"""docstring for Datanode"""
-	def __init__(self, arg):
+	def __init__(self, chunkloc):
 		super(Datanode, self).__init__()
-		self.arg = arg
+		self.chunkloc = chunkloc
+		self.local_fs_root = "D:/HDFSTemp/Datanode" + str(chunkloc)
+		if not os.path.isdir(self.local_fs_root):
+			os.makedirs(self.local_fs_root)
+
 	def write(self, chunk_uuid, chunk):
-		with open(str(chunk_uuid), "w") as fw:
+		with open(self.local_fs_root + "/" + str(chunk_uuid), "w") as fw:
 			fw.write(chunk)
 	
 	def read(self, chunk_uuid):
 		data = None
-		with open(str(chunk_uuid), "r") as fr:
+		with open(self.local_fs_root + "/" + str(chunk_uuid), "r") as fr:
 			data = fr.read()
 		return data
 
@@ -17,8 +23,8 @@ def main():
 	# test
 	i = 2
 	t = Datanode(i)
-	t.write(123, "Hello world.")
-	print t.read(123)
+	t.write(1234, "Hello world.")
+	print t.read(1234)
 
 
 if __name__ == '__main__':
