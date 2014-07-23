f061184f0cd647a4a283dfb9744bdbbd8723a90c
DanielJyc <496830205@qq.com>
2014-07-23 13:22:57 +0800

    Delete useless/test lines.

--- HDFS.py
+++ HDFS.py
@@ -16,8 +16,6 @@ class Client(object):
 		for i in range(0, len(data), self.namenode.chunksize):
 			chunks.append(data[i:i+self.namenode.chunksize])
 		chunk_uuids = self.namenode.alloc(filename, num_chunks) #为文件分配空间，完成元数据	
-		# print chunk_uuids
-		# print chunks
 		for i in range(0, len(chunk_uuids)):			
 			chunkloc = i % self.namenode.num_datanodes + 1
 			self.namenode.datanodes[chunkloc].write(chunk_uuids[i], chunks[i]) 	
@@ -29,11 +27,8 @@ class Client(object):
 		if True == self.namenode.exits(filename) :
 			data = ''
 			chunk_uuids = self.namenode.filetable[filename]
-			# print chunk_uuids
 			for chunk_uuid in chunk_uuids:
 				chunkloc = self.namenode.chunktable[chunk_uuid] #获取uuid的DataNode的位置
-				# print chunk_uuid
-				# print chunkloc
 				data_temp = self.namenode.datanodes[chunkloc].read(chunk_uuid)	
 				if -1 == data_temp: #读取当前DataNode上的chunk不存在（即：某一个DataNode被损坏）
 					data_temp = self.namenode.datanodes[chunkloc%self.namenode.num_datanodes + 1].read(chunk_uuid)
@@ -86,18 +81,13 @@ class Namenode(object):
 			chunkloc = chunkloc % self.num_datanodes + 1 #！！注意：要+1，否则chunkloc值不会变坏
 		self.filetable[filename] = chunk_uuids
 		print self.filetable
-		# print self.chunktable
 		return chunk_uuids
 
 	def delete(self, filename):
-		# print self.filetable
-		# print self.chunktable		
 		chunk_uuids = self.filetable[filename]
 		for chunk_uuid in chunk_uuids:
 			self.chunktable.pop(chunk_uuid)
 		self.filetable.pop(filename)
-		# print self.filetable
-		# print self.chunktable
 
 	def exits(self, filename): #检测文件是否存在
 		if filename in self.filetable:
@@ -158,7 +148,6 @@ class Command(object):
 
 	def upload_cmd(self):
 		filename = raw_input('Input the filename which you want to upload in local:\n')
-		# data = raw_input('Input your data:\n')
 		try :
 			with open(filename, "r") as fr: #读取本地文件
 				data = fr.read()
@@ -174,63 +163,10 @@ class Command(object):
 			fw.write(data)	  #写入本地
 
 def main():		
-	# 4. command_line()
-	# command_line()
 	nd = Namenode()
 	client = Client(nd)	
 	command = Command(client)
 	command.command_line()
 
-	#3.test for Client
-	# nd = Namenode()
-	# c = Client(nd)
-	# c.delete("jyc1")
-	# c.read("jyc1")
-	# c.write("jyc1", "Hello jyc1.Hello jyc1.Hello jyc1.Hello jyc1.")
-	# # time.sleep(5)
-	# c.write("jyc2", "Hello jyc2.Hello jyc2.Hello jyc2.Hello jyc2.")
-	# c.write("jyc3", "Hello jyc3.Hello jyc3.Hello jyc3.Hello jyc3.")
-	# # time.sleep(5)
-	# print c.read("jyc1")
-	# print c.read("jyc2")
-	# print c.read("jyc3")
-	# print nd.filetable
-	# c.list_files()
-	# c.delete("jyc2")
-	# print nd.filetable
-	# c.list_files()
-
-	#2.test for Namenode
-	# nd = Namenode()
-	# print type(nd.datanodes[1])
-	# print len(nd.alloc("jyc1.txt", 1))
-	# print len(nd.alloc("jyc2.txt", 2))
-	# print len(nd.alloc("jyc3.txt", 3))
-	# nd.delete("jyc3.txt")
-
-	# 1.test for Datanode
-	# i = 2
-	# t = Datanode(i)
-	# t.write(1234, "Hello world.")
-	# print t.read(1234)
-	# time.sleep(2)	#延迟两秒
-	# t.delete(1234)
-
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
 	main()
-
-
-		
-		
